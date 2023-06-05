d40184b1628f5db9b4dc0adbe2a82c28936ddafe
Nurmira Zhorobek kyzy <97220234+Nurmira16@users.noreply.github.com>
2023-06-05 14:58:40 +0200

    feat: setInterval

--- mole.js
+++ mole.js
@@ -1,3 +1,16 @@
+const MIN_INTERVAL = 2000;
+const MAX_INTERVAL = 20000;
+const SAD_INTERVAL = 500;
+const HUNGRY_INTERVAL = 2000;
+const wormContainer = document.querySelector(".worm-container");
+let score = 0;
+
+const getInterval = () =>
+  Date.now() + MIN_INTERVAL + Math.floor(Math.random() * MAX_INTERVAL);
+const getSadInterval = () => Date.now() + SAD_INTERVAL;
+const getKingStatus = () => Math.random() > 0.9;
+const getHungryInterval = () => Date.now() + HUNGRY_INTERVAL;
+
 const moles = [
   {
     status: "sad",
